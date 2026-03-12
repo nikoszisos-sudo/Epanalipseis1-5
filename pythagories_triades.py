be7d7bb50636de74436be099838257c1e0d60c38
Nikolaos Zisos <nikoszisos@gmail.com>
2026-03-12 11:54:50 +0200

    Delete pythagories_triades.py

--- pythagories_triades.py
+++
@@ -1,6 +0,0 @@
-for a in range(0, 21, 1):
-    for b in range(0, 21, 1):
-        for c in range(0, 21, 1):
-            if (a*a + b*b) == c*c:
-              print(str(a) + " " + str(b) + " " + str(c))
-
